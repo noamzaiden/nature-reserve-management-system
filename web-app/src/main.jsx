14cc6bc3224f30361c6bd3b5fa82766a140ed08f
Noam Zaiden <noamzaiden@gmail.com>
2026-04-26 18:35:53 +0200

    Polish compact web app theme

--- web-app/src/main.jsx
+++ web-app/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import App from './App'
 import './styles.css'
+import './final-theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
